b51618b57ce563ca8e95a210b84449f6e752fcbb
Pokegear <christoph.ding@gmail.com>
2016-11-19 15:10:38 -0500

    Add functions and requiring other functions to script that do the data sets in order.

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-// read the inputs
 
-// build the relief
+function createRadarChart () {
+  // read the inputs
+  var dataSets = require('./inputs/data.json');
+  var configurations = require('./inputs/style.json');
 
-// for each dataset
-  // put that dataset into the chart
-  
+  // create the canvas element, legend and axis
+  var maximum = calculateChartBounds(dataSets.data);
+  require('./buildRelief.js').buildRelief(maximum, configurations['relief']);
+
+  // visualize the datasets on the chart
+  require('./chartSingleDataset.js').visualizeData(dataSets, configurations['data']);  
+}
+
+// 
+function calculateChartBounds (dataSets) {
+  var max = 0;
+  // iterate through dataSets, looking for a maximum value to determine range
+
+  return max;
+}
+
+// run the program
+createRadarChart();
+
