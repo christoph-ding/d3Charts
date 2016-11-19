3e97100a3c8d88c75e306da498354f98670e1038
Pokegear <christoph.ding@gmail.com>
2016-11-19 14:50:08 -0500

    Make stub functions for building the entire canvas, and for visualizing the datasets.

---
+++ buildRelief.js
@@ -0,0 +1,14 @@
+// Make the canvas
+
+
+// Make the legend
+
+
+// Make the axis and labels
+
+
+// Make rings 
+
+
+// build the entire relief - this is exposed
+  // call each with correct option
---
+++ chartSingleDataset.js
@@ -0,0 +1,7 @@
+// make a single point
+
+
+// make a single spline
+
+
+// for all the attributes: value pairs in the dataset, visualize that pair on the chart - this is exposed
