ad2c579deb38edfdf5d80abf7ddc876151fc6112
Pokegear <christoph.ding@gmail.com>
2016-11-20 17:23:20 -0500

    Make functions that puts node using data onto radar.  Added different colors for each dataset.

--- script.js
+++ script.js
@@ -17,31 +17,36 @@ function createRadarChart () {
      {"axis": "coolness", "value": 11},
      {"axis": "awesomeness", "value": 11}
     ],
-    [{"axis": "hit points", "value": 18},
-     {"axis": "magic points", "value": 17}, 
+    [{"axis": "hit points", "value": 19},
+     {"axis": "magic points", "value": 24}, 
      {"axis": "wisdom", "value": 16},
      {"axis": "strength", "value": 15},
-     {"axis": "agility", "value": 14},
-     {"axis": "charisma", "value": 13},
+     {"axis": "agility", "value": 24},
+     {"axis": "charisma", "value": 33},
      {"axis": "toughness", "value": 12},
      {"axis": "dexterity", "value": 11},
-     {"axis": "cleverness", "value": 11},
-     {"axis": "money", "value": 11},
-     {"axis": "coolness", "value": 11},
-     {"axis": "awesomeness", "value": 11}
+     {"axis": "cleverness", "value": 21},
+     {"axis": "money", "value": 25},
+     {"axis": "coolness", "value": 20},
+     {"axis": "awesomeness", "value": 19}
     ]]
   };
 
   config = {
   };
 
-  // // create the canvas element, legend and axis
+  var fills = ['#0C7489', '#931F1D'];
+
+  // create the canvas element, legend and axis
   var maximum = calculateChartBounds(dataSets.data);
   var axisLabels = gatherAxisLabels(dataSets.data[0]);
-  buildRelief(maximum, axisLabels)
+  buildRelief(maximum, axisLabels);
 
-  // // visualize the datasets on the chart
-  // require('./chartSingleDataset.js').visualizeData(dataSets, configurations['data']);  
+  // visualize the datasets on the chart
+  // 
+  for (var i = 0; i < dataSets.data.length; i++) {
+    visData(dataSets.data[i], 850, 600, maximum, fills[i]);
+  }
 }
 
 // 
