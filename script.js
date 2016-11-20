954c83d5c94b1352b40feefe6b913c3d6396dd24
Pokegear <christoph.ding@gmail.com>
2016-11-20 13:18:08 -0500

    Define function that determines maximum axis.  Change test data.

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-
 function createRadarChart () {
   // read the inputs
   var dataSets = require('./inputs/data.json');
   var configurations = require('./inputs/style.json');
 
-  // create the canvas element, legend and axis
+  // // create the canvas element, legend and axis
   var maximum = calculateChartBounds(dataSets.data);
-  require('./buildRelief.js').buildRelief(maximum, configurations['relief']);
+  console.log(maximum);
+  // var axisLabels = gatherAxisLabels();
+  // require('./buildRelief.js').buildRelief(maximum, configurations['relief']);
 
-  // visualize the datasets on the chart
-  require('./chartSingleDataset.js').visualizeData(dataSets, configurations['data']);  
+  // // visualize the datasets on the chart
+  // require('./chartSingleDataset.js').visualizeData(dataSets, configurations['data']);  
 }
 
 // 
-function calculateChartBounds (dataSets) {
+function calculateChartBounds (dataSets) {  
   var max = 0;
   // iterate through dataSets, looking for a maximum value to determine range
-
+  for (var i = 0; i < dataSets.length; i++) {
+    currentDataSet = dataSets[i];
+    for (var j = 0; j < currentDataSet.length; j++) {
+      if (currentDataSet[j].value > max) {
+        max = currentDataSet[j].value;
+      }
+    }
+  }
   return max;
+}
+
+function gatherAxisLabels(dataset) {
+  var labels = [];
+  return labels;
 }
 
 // run the program
 createRadarChart();
-
