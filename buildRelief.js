f8f68fd44e5631ea6ea1bdcea7c066f5149be9a1
Pokegear <christoph.ding@gmail.com>
2016-11-20 14:33:29 -0500

    Add function that generates a really simple legend.

--- buildRelief.js
+++ buildRelief.js
@@ -2,23 +2,36 @@
 function makeCanvas (width, height) {
   console.log('making canvas...');
   // g is a group tag
-  var canvas = d3.select("#body")
-      .append("svg")
+  var canvas = d3.select('#body')
+      .append('svg')
       .attr('width', width)
       .attr('height', height)
       .style('background', '#FFEEDD')
-      .append("g");
+      .append('g');
 
 }
 
 // Make the legend
 function insertLegend () {
+  console.log('making legend...');
+
+  var legend = d3.select('#body')
+      .append('svg')
+      .attr('width', 100)
+      .attr('height', 40)
+      .style('background', '#B8B8FF');
+
+  var text = legend.append('text')
+      .attr("x", 0)
+      .attr("y", 10)
+      .attr("font-size", "14px")
+      .text("Legend");
 
 }
 
 // Make the axis and labels
 function labelAxis (labels, maximum) {
-
+  
 }
 
 // Make rings 
@@ -36,7 +49,7 @@ function makeRings (size) {
 function buildRelief (maximum) {
   console.log('building relief...');
   makeCanvas(500, 500);
-  // insertLegend();
+  insertLegend();
   // labelAxis();
   // makeRings();
 }
